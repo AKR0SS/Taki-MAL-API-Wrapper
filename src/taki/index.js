56642f51f4880ec3a54a1b05ef48e326ba15d56f
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-01-28 14:42:07 -0500

    Added method getSeason()

--- src/taki/index.js
+++ src/taki/index.js
@@ -1,10 +1,13 @@
-const { setClientKey, getInfoFromId, getInfoFromURL } = require('./modules/info.js');
+const { getInfoFromId, getInfoFromURL } = require('./modules/info.js');
 const { getInfoFromName, search } = require('./modules/search.js');
+const { getSeason } =  require('./modules/season.js');
+const { setClientKey } = require('./handlers/clientHandler.js');
 
 module.exports = {
     setClientKey,
     getInfoFromId,
     getInfoFromURL,
     getInfoFromName,
-    search
+    search,
+    getSeason
 };
