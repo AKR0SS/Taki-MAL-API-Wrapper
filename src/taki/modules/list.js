3e5e3611eb5958959edd726d29aa2fadb46b828b
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-01-29 14:52:03 -0500

    added getUserWatchList jsdoc

--- src/taki/modules/list.js
+++ src/taki/modules/list.js
@@ -1,6 +1,11 @@
 const getList = require("../handlers/listHandler");
 const { checkClientKey } = require('../handlers/clientHandler');
 
+/**
+ * Promises a json data object provided a username
+ * @param {String} user
+ * @returns {Promise} `User Anime List Model`
+ */
 function getUserWatchList(user) {
     return new Promise(async (resolve, reject) => {
         if (!user || typeof user !== 'string') return reject(new Error('[TAKI] Invalid User'));
