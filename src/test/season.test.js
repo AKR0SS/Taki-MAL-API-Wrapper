3a26abf6adff8a74ef3abe97cbf158b0e22af750
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-01-29 13:23:33 -0500

    rejection tests for getSeason parameters

--- src/test/season.test.js
+++ src/test/season.test.js
@@ -7,6 +7,20 @@ const season = 'spring';
 const year = 2015;
 const animeId = 27775;
 
+// Exception Testing
+
+test('rejects an invalid year', () => {
+    expect(getSeason(season, '2015')).rejects.toThrow('[TAKI] Invalid Year');
+    expect(getSeason(season, '')).rejects.toThrow('[TAKI] Invalid Year');
+});
+
+test('rejects an invad season', () => {
+    expect(getSeason(1, year)).rejects.toThrow('[TAKI] Invalid Season');
+    expect(getSeason('', year)).rejects.toThrow('[TAKI] Invalid Season');
+});
+
+// Logic Testing
+
 test('Get Seasonal Data', async () => {
     setClientKey(CLIENT_KEY);
     const data = await getSeason(season, year);
