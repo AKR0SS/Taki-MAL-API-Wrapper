1e8f9c7fc77d2739ed28c9419329b5c7fe9fa7e1
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-01-29 13:22:43 -0500

    added rejects to getSeason

--- src/taki/modules/season.js
+++ src/taki/modules/season.js
@@ -3,10 +3,14 @@ const getSeasonInfo = require('../handlers/seasonHandler');
 /**
  * Promises an array of json data object provided an anime's NAME
  * @param {string} season
+ * @param {number} year
  * @return {Promise} `Anime Season Model`
  */
 function getSeason(season, year) {
     return new Promise(async (resolve, reject) => {
+        if (!year || typeof year !== 'number') return reject(new Error('[TAKI] Invalid Year'));
+        if (!season || typeof season !== 'string') return reject(new Error('[TAKI] Invalid Season'));
+
         let data = await getSeasonInfo(season, year);
         resolve(data);
     });
