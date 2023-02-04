066462d2e4d7acbaa329bc751c2f18802f6c2445
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2023-02-04 15:03:39 -0500

    added pagination

--- src/taki/modules/season.js
+++ src/taki/modules/season.js
@@ -1,5 +1,6 @@
 const getSeasonInfo = require('../handlers/seasonHandler');
 const { checkClientKey } = require('../handlers/clientHandler');
+const { getNext, getPrevious } = require('../handlers/pagingHandler');
 
 /**
  * Promises an array of json data object provided an anime's NAME
@@ -8,6 +9,36 @@ const { checkClientKey } = require('../handlers/clientHandler');
  * @return {Promise} `Anime Season Model`
  */
 function getSeason(season, year) {
+  /** 
+   * Promises a json data object provided a username
+   * @param {Object} data
+   * @returns {Promise} `User Anime List Model`
+   */
+  getSeason.next = (data) => {
+    return new Promise(async (resolve, reject) => {
+      if (!data || typeof data !== 'object') return reject(new Error('[TAKI] Invalid Data'));
+      if (!checkClientKey()) return reject(new Error('[TAKI] No MAL "CLIENT_KEY" provided'));
+      if (!data.paging.next) return reject(new Error('[TAKI] Cannot access page that does not exist'));
+
+      resolve(getNext(data));
+    });
+  };
+
+  /** 
+   * Promises a json data object provided a username
+   * @param {Object} data
+   * @returns {Promise} `User Anime List Model`
+   */
+  getSeason.previous = (data) => {
+    return new Promise(async (resolve, reject) => {
+      if (!data || typeof data !== 'object') return reject(new Error('[TAKI] Invalid Data'));
+      if (!checkClientKey()) return reject(new Error('[TAKI] No MAL "CLIENT_KEY" provided'));
+      if (!data.paging.previous) return reject(new Error('[TAKI] Cannot access page that does not exist'));
+
+      resolve(getPrevious(data));
+    });
+  };
+
   return new Promise(async (resolve, reject) => {
     if (!year || typeof year !== 'number') return reject(new Error('[TAKI] Invalid Year'));
     if (!season || typeof season !== 'string') return reject(new Error('[TAKI] Invalid Season'));
